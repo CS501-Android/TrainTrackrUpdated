5f1de0ae991c4e9b29a77df8db7d5bfd3cd58ada
Hou chi Chan <houchichan@crc-dot1x-nat-10-239-142-208.bu.edu>
2023-12-12 18:29:37 -0500

    update import

--- app/src/main/java/com/example/trainfinal/RetrofitClientInstance.kt
+++ app/src/main/java/com/example/trainfinal/RetrofitClientInstance.kt
@@ -1,5 +1,9 @@
 package com.example.trainfinal
 
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
 object RetrofitClientInstance {
     private const val BASE_URL = "https://api.sncf.com/v1/"
 
@@ -11,7 +15,7 @@ object RetrofitClientInstance {
                 val original = chain.request()
                 val request = original.newBuilder()
                     .header("Authorization", "b12f312b-64fe-4ea2-adb8-6aa05b92815f")
-                    .method(original.method(), original.body())
+                    .method(original.method, original.body)
                     .build()
                 chain.proceed(request)
             }.build())
