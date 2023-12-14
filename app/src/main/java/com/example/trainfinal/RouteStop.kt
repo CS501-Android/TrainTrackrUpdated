2695e16dc552883af18dcdeb14fc291ab4de8c69
Hou chi Chan <houchichan@Hous-MacBook-Air.local>
2023-12-13 19:02:13 -0500

    finished map features

--- app/src/main/java/com/example/trainfinal/RouteStop.kt
+++ app/src/main/java/com/example/trainfinal/RouteStop.kt
@@ -8,12 +8,20 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.EditText
 import androidx.fragment.app.activityViewModels
+import android.location.Address
+import android.location.Geocoder
+import android.text.TextUtils
+import android.util.Log
+import java.io.IOException
 
+@Suppress("DEPRECATION")
 class RouteStop : Fragment() {
     private lateinit var title: EditText
     private lateinit var lat: EditText
     private lateinit var long: EditText
+    private lateinit var search: EditText
     private lateinit var submitBtn: Button
+    private lateinit var searchBtn: Button
     private val viewModel: StopViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -28,9 +36,16 @@ class RouteStop : Fragment() {
         title = view.findViewById(R.id.input_field_title)
         lat = view.findViewById(R.id.input_field_lat_routes)
         long = view.findViewById(R.id.input_field_long_routes)
+        search = view.findViewById(R.id.search)
         submitBtn = view.findViewById(R.id.submit_stop)
+        searchBtn = view.findViewById(R.id.searchLocation)
 
         submitBtn.setOnClickListener {
+            // Empty Check
+            if (TextUtils.isEmpty(title.text.toString())) return@setOnClickListener
+            if (TextUtils.isEmpty(lat.text.toString())) return@setOnClickListener
+            if (TextUtils.isEmpty(long.text.toString())) return@setOnClickListener
+
             var stopList: ArrayList<RouteStops>? = ArrayList()
             if (viewModel.getData() != null) stopList = viewModel.getData()
             val newStop = RouteStops(
@@ -41,8 +56,27 @@ class RouteStop : Fragment() {
             title.setText("")
             lat.setText("")
             long.setText("")
+            search.setText("")
             stopList?.add(newStop)
             viewModel.updateData(stopList!!)
+        }
+
+        searchBtn.setOnClickListener {
+            if (TextUtils.isEmpty(search.text.toString())) return@setOnClickListener
+            val geoCoder = Geocoder(requireActivity())
+
+            var addressList: List<Address>? = null
+            try {
+                addressList = geoCoder.getFromLocationName(search.text.toString(), 1)
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+            val address = addressList!![0]
+            title.setText(search.text.toString())
+            lat.setText(address.latitude.toString())
+            long.setText(address.longitude.toString())
+
+            Log.i("firebasestupid", address.toString())
         }
 
         return view
